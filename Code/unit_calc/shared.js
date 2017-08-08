9a0cfe31b97a7c44b8c12ca4c6f1a8ae07242826
Avery Scott Norris <averyscottnorris@AN-Mac.local>
2017-08-08 15:32:00 -0700

    Updated IBU calculator to use CSS file

--- Code/unit_calc/shared.js
+++ Code/unit_calc/shared.js
@@ -1,162 +1,162 @@
 /*
-File Name: shared.js
-Authors: Cody Leytham & Avery Norris	Date:8/1/17
-Purpose: helper functions for the  html javscript functions.
-*/
+   File Name: shared.js
+   Authors: Cody Leytham & Avery Norris	Date:8/1/17
+   Purpose: helper functions for the  html javscript functions.
+   */
 
 
 //helper functions used in all many files
 
 function isFloat(src){
-//this will determine if the input is a floating point number
-  var dec;
-  dec = 0;
-  for(i = 0; i < src.length; i++){
-  //makind a decimal count, it will only allow 1 decimal in the input
-	//looping through the string seeing if there are any non-numeric or '.' elements
-	if(isNaN(parseInt(src[i], 10)) && src[i] != "."){
-		//if the first character is a - then permitting it for negative numbers
-		if(i == 0 && src[i] == '-')
-			break;
-		return 0;	
-	}
-	if(isNaN(parseInt(src[i], 10)) && src[i] == "."){
-/*
-counting the decimals in the number, if it is more than 1, then rejecting I am american,
- so I designed this to follow the paradigm that , are are put inbetween sets of 3 numbers
- aka the radix character.
- ex: 1,000,000. is 1 million, not 1.000.000,
-*/
-		//
-		dec++;
-		if(dec > 1){
-			return 0;
-		}
-	}
-  }
-  return 1;
+    //this will determine if the input is a floating point number
+    var dec;
+    dec = 0;
+    for(i = 0; i < src.length; i++){
+        //makind a decimal count, it will only allow 1 decimal in the input
+        //looping through the string seeing if there are any non-numeric or '.' elements
+        if(isNaN(parseInt(src[i], 10)) && src[i] != "."){
+            //if the first character is a - then permitting it for negative numbers
+            if(i == 0 && src[i] == '-')
+                break;
+            return 0;	
+        }
+        if(isNaN(parseInt(src[i], 10)) && src[i] == "."){
+            /*
+               counting the decimals in the number, if it is more than 1, then rejecting I am american,
+               so I designed this to follow the paradigm that , are are put inbetween sets of 3 numbers
+               aka the radix character.
+               ex: 1,000,000. is 1 million, not 1.000.000,
+               */
+            //
+            dec++;
+            if(dec > 1){
+                return 0;
+            }
+        }
+    }
+    return 1;
 }
 
 function isInt(src){
-//this will determine if the input is a whole point number
-  for(i = 0; i < src.length; i++){
-	//looping through the string seeing if there are any non-numeric elements
-	if(isNaN(parseInt(src[i], 10))){
-		//if the first character is a - then permitting it for negative numbers
-		if(i == 0 && src[i] == '-')
-			break;
-		return 0;	
-	}
-  }
-  return 1;
+    //this will determine if the input is a whole point number
+    for(i = 0; i < src.length; i++){
+        //looping through the string seeing if there are any non-numeric elements
+        if(isNaN(parseInt(src[i], 10))){
+            //if the first character is a - then permitting it for negative numbers
+            if(i == 0 && src[i] == '-')
+                break;
+            return 0;	
+        }
+    }
+    return 1;
 }
 
 function checkLen(src, lim){
-//check to see if the length is at or bellow the allowed length (lim)
-  if(src.length >= lim){
-    return 0;
-  }
-  return 1;
+    //check to see if the length is at or bellow the allowed length (lim)
+    if(src.length >= lim){
+        return 0;
+    }
+    return 1;
 }
 
 function scrubInt(src, lim){
-//this will scrub integer input to make sure its an int of the right length
-//lim is used to specify the length 
- if(!checkLen(src, lim) && !isInt(src))
-    //the input is not an int
-    return 3;
-  else if(checkLen(src, lim) && isInt(src))
-    //the input is an int the right size
-    return 0;
-  else if(!isInt(src))
-    //the input is not an int
-    return 1;
-  else if(!checkLen(src, lim))
-    //the input is to long
-    return 2;
-  else
-    return 4;
+    //this will scrub integer input to make sure its an int of the right length
+    //lim is used to specify the length 
+    if(!checkLen(src, lim) && !isInt(src))
+        //the input is not an int
+        return 3;
+    else if(checkLen(src, lim) && isInt(src))
+        //the input is an int the right size
+        return 0;
+    else if(!isInt(src))
+        //the input is not an int
+        return 1;
+    else if(!checkLen(src, lim))
+        //the input is to long
+        return 2;
+    else
+        return 4;
 }
 
 function scrubFloat(src, lim){
-//this will scrub integer input to make sure its an int of the right length
-//lim is used to specify the length 
-  if(!checkLen(src, lim) && !isFloat(src))
-    //the input is to large and not a float
-    return 3;
-  else if(checkLen(src, lim) && isFloat(src))
-    //the input is a float and the right size
-    return 0;
-  else if(!isFloat(src))
-    //the input is not a float
-    return 1;
-  else if(!checkLen(src, lim))
-    //the input is to long
-    return 2;
-  else
-    //i hope this will not be reached
-    return 4;
+    //this will scrub integer input to make sure its an int of the right length
+    //lim is used to specify the length 
+    if(!checkLen(src, lim) && !isFloat(src))
+        //the input is to large and not a float
+        return 3;
+    else if(checkLen(src, lim) && isFloat(src))
+        //the input is a float and the right size
+        return 0;
+    else if(!isFloat(src))
+        //the input is not a float
+        return 1;
+    else if(!checkLen(src, lim))
+        //the input is to long
+        return 2;
+    else
+        //i hope this will not be reached
+        return 4;
 }
 
 function testEmpty(src){
-  if(src.length == 0){
-    alert("There is an empty field.");
-    return 1;
-  }
-  return 0;
+    if(src.length == 0){
+        alert("There is an empty field.");
+        return 1;
+    }
+    return 0;
 }
 
 function dryConv(mass, from, to){
-//  1 pound = 16oz| 1 kg = 16pounds|  1 kg = 35.2oz| 1 pound = .4545 kg (rounded to .455)|
-  var res;
-  if(from == 1 && to == 2){
-    //kg to pounds
-    res = mass*2.2;
-  }
-  else if(from == 1 && to == 3){
-    //kg to oz
-    res = mass*35.2;
-  }
-  else if(from == 2 && to == 1){
-    //pounds to killograms
-    res = mass*.455;
-  }
-  else if(from == 2 && to == 3){
-    //pounds to ounces
-    res = mass*16;
-  }
-  else if(from == 3 && to == 1){
-    //ounces to kg
-    res = mass/35.2;
-  }
-  else if(from == 3 && to == 2){
-    //pounds to ounces
-    res = mass/16;
-  }
-  else
-    return mass;
-  return (Math.trunc(res*100))/100;
+    //  1 pound = 16oz| 1 kg = 16pounds|  1 kg = 35.2oz| 1 pound = .4545 kg (rounded to .455)|
+    var res;
+    if(from == 1 && to == 2){
+        //kg to pounds
+        res = mass*2.2;
+    }
+    else if(from == 1 && to == 3){
+        //kg to oz
+        res = mass*35.2;
+    }
+    else if(from == 2 && to == 1){
+        //pounds to killograms
+        res = mass*.455;
+    }
+    else if(from == 2 && to == 3){
+        //pounds to ounces
+        res = mass*16;
+    }
+    else if(from == 3 && to == 1){
+        //ounces to kg
+        res = mass/35.2;
+    }
+    else if(from == 3 && to == 2){
+        //pounds to ounces
+        res = mass/16;
+    }
+    else
+        return mass;
+    return (Math.trunc(res*100))/100;
 }
 
 function wetConv(vol, from, to){
-  if(from == 1 && to == 2)
-    res =  vol*.264;
-  else if(from == 2 && to == 1)
-    res = vol/.264;
-  else
-    return vol;
-  return (Math.trunc(res*100))/100;
+    if(from == 1 && to == 2)
+        res =  vol*.264;
+    else if(from == 2 && to == 1)
+        res = vol/.264;
+    else
+        return vol;
+    return (Math.trunc(res*100))/100;
 }
 
 function scrub(src, len){
-  var check = scrubFloat(src, 10);
-  if(check == 1)
-    alert("Some input is not a number...");
-  else if(check == 2)
-    alert("Some input is to large");
-  else if(check == 3)
-    alert("Some input is too long and not a number");
-  return check;
+    var check = scrubFloat(src, 10);
+    if(check == 1)
+        alert("Some input is not a number...");
+    else if(check == 2)
+        alert("Some input is to large");
+    else if(check == 3)
+        alert("Some input is too long and not a number");
+    return check;
 }
 
 
@@ -168,45 +168,45 @@ if(unitConvButton) {
 }
 
 function unit_conv(){
-  //getting the data from the html
-  var value = document.getElementById("g1").value; 
-  var gf1 = document.getElementById("gf1").value; 
-  var gt1 = document.getElementById("gt1").value;
-  var fname = document.getElementById('gf1').options[document.getElementById('gf1').selectedIndex].text;
-  var tname = document.getElementById('gt1').options[document.getElementById('gt1').selectedIndex].text;
-  var res = 1;
-  //this will check to make sure that the input is a float at most 10 long (decimal included).
-  var check = scrub(value, 10);
-  //if the check does not return an error, then checking if it is positive.
-  if(check == 0){
-    if(value > 0){
-      //if the there is no error and it is a positive numer, doing the calculations and returning the results.
-      res = dryConv(value, gf1, gt1);
-      document.getElementById("grain1").innerHTML = value + " " + fname + " is " + res + " " + tname;
+    //getting the data from the html
+    var value = document.getElementById("g1").value; 
+    var gf1 = document.getElementById("gf1").value; 
+    var gt1 = document.getElementById("gt1").value;
+    var fname = document.getElementById('gf1').options[document.getElementById('gf1').selectedIndex].text;
+    var tname = document.getElementById('gt1').options[document.getElementById('gt1').selectedIndex].text;
+    var res = 1;
+    //this will check to make sure that the input is a float at most 10 long (decimal included).
+    var check = scrub(value, 10);
+    //if the check does not return an error, then checking if it is positive.
+    if(check == 0){
+        if(value > 0){
+            //if the there is no error and it is a positive numer, doing the calculations and returning the results.
+            res = dryConv(value, gf1, gt1);
+            document.getElementById("grain1").innerHTML = value + " " + fname + " is " + res + " " + tname;
+        }
+        else if(value < 0)
+            alert("Please only use positive number. NO SPATIAL SINGULARITIES!!!.");
     }
-    else if(value < 0)
-      alert("Please only use positive number. NO SPATIAL SINGULARITIES!!!.");
-  }
 
-  //getting the data from the html
-  var value = document.getElementById("l1").value; 
-  var fname = document.getElementById('lf1').options[document.getElementById('lf1').selectedIndex].text;
-  var tname = document.getElementById('lt1').options[document.getElementById('lt1').selectedIndex].text;
-  var lf1 = document.getElementById("lf1").value; 
-  var lt1 = document.getElementById("lt1").value;
-  var res = 1;
-  //this will check to make sure that the input is a float at most 10 long (decimal included).
-  var check = scrub(value, 10);
-  //if the check does not return an error, then checking if it is positive.
-  if(check == 0){
-    if(value > 0){
-      //if the there is no error and it is a positive numer, doing the calculations and returning the results.
-      res = wetConv(value, lf1, lt1);
-      document.getElementById("liquid1").innerHTML = value + " " + fname + " is " + res + " " + tname;
+    //getting the data from the html
+    var value = document.getElementById("l1").value; 
+    var fname = document.getElementById('lf1').options[document.getElementById('lf1').selectedIndex].text;
+    var tname = document.getElementById('lt1').options[document.getElementById('lt1').selectedIndex].text;
+    var lf1 = document.getElementById("lf1").value; 
+    var lt1 = document.getElementById("lt1").value;
+    var res = 1;
+    //this will check to make sure that the input is a float at most 10 long (decimal included).
+    var check = scrub(value, 10);
+    //if the check does not return an error, then checking if it is positive.
+    if(check == 0){
+        if(value > 0){
+            //if the there is no error and it is a positive numer, doing the calculations and returning the results.
+            res = wetConv(value, lf1, lt1);
+            document.getElementById("liquid1").innerHTML = value + " " + fname + " is " + res + " " + tname;
+        }
+        else if(value < 0)
+            alert("Please only use positive number. NO SPATIAL SINGULARITIES!!!.");
     }
-    else if(value < 0)
-      alert("Please only use positive number. NO SPATIAL SINGULARITIES!!!.");
-  }
 }
 
 //Functions for the IBU calculator
@@ -222,12 +222,44 @@ function ibu_calculate() {
     var alpha_acid = document.getElementById("aa").value;
     var volume = document.getElementById("vol").value;
     var time = document.getElementById("time").value;
+    var gravity = document.getElementById("gravity").value;
+
+    //check for valid input
+    if(oz_hops == 0 || alpha_acid == 0 || volume == 0 || time == 0) {
+        alert("Invalid values!");
+        return false;
+    }
+
+    //if no gravity entered, give a generic, middle-of-the-road gravity
+    if(!gravity) {
+        gravity = 0.080;
+    }
+    else {
+        gravity = gravity - 1;
+    }
+
+    alpha_acid = alpha_acid * 0.01;
+
+    //variables for calculations
+    var IBUs = 0;
+    var acid_utilization = 0;
+    var added_acids = 0;
+    var bigness = 0;
+    var boil_factor = 0;
 
     //Run IBU calculations
-    
+    added_acids = (alpha_acid * oz_hops * 7490) / volume;
+    bigness = 1.65 * Math.pow(0.000125, gravity);
+    boil_factor = (1 - Math.exp(-0.04 * time)) / 4.15;
+    acid_utilization = bigness * boil_factor;
+    IBUs = added_acids * acid_utilization;
+
 
     //Output result to page
-    document.getElementById("ibu_output").innerHTML = 1234;
+    //if all fields are filled
+    document.getElementById("ibu_output").innerHTML = (Math.round(IBUs * 10) / 10) + " IBUs";
+
+    return true;
 }
 
 
